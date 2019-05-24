b0b89abe5be924cda1e1709b18c212accfeb24a8
apsara80 <jenny.mayo@gmail.com>
2019-05-24 09:40:31 -0600

    Coursera data science week 3 assignment: Two functions have been added to take a matrix object and calculate its inverse. If the inverse has been done already, it caches the result to be called upon later without calculating it again.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is a notoriously cumbersome computation and therefore it can save
+## time and memory space by caching the inverse of a matrix rather than computing it
+## each time repeatedly. The following pair of functions cache the inverse of 
+## matrix, using lexical scoping in R.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## It 'sets' and 'gets' a matrix object to be passed down to the next fuction, cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function (y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list (set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## `makeCacheMatrix` above. If the inverse matrix has already been calculated,
+## then this function retrieves the inverse from the cache. 
+## If the inverse hasn't been calculated, this function calculates it using 'solve()'
+## and returns the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        myMatrix <- x$get()
+        inv <- solve (myMatrix, ...)
+        x$setinv(inv)
+        inv
 }
+
